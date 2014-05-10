93fd1b7a365e5e54f3695de139829eeb1c78b02e
monkey-krk <monkey@monkey.com.hr>
2014-05-10 15:48:40 +0200

    Profile-New-approach-F3Tabs

---
+++ LifeDojo/Assets/Js/Profile-New-approach.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../External/jQuery/jquery-2.1.0.min.js" />
+
+$(document).ready(function () {
+
+    if ($("#JournalText").length > 0){
+        $("#JournalText").redactor({
+            toolbarExternal: '#EditorToolBar'
+        });
+    }
+
+
+
+
+
+});
--- LifeDojo/Assets/Js/shared.js
+++ LifeDojo/Assets/Js/shared.js
@@ -1,6 +1,24 @@
 ﻿/// <reference path="../External/jQuery/jquery-2.1.0.min.js" />
 
+function OpenTab(tab) {
+    $(".TTab.active").removeClass("active");
+    $(".TTab[data-tab='" + tab + "']").addClass("active");
+    $(".TContent.active").removeClass("active");
+    $(".TContent[data-tab='" + tab + "']").addClass("active");
+}
+
 $(document).ready(function () {
+    
+    if ($(".Tabs").length > 0) {
+        $(".TTab").click(function (e) {
+            OpenTab($(this).attr("data-tab"));
+            return false;
+        });
+    }
+
+
+
+
 
 
 });
