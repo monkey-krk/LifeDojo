2fb95aa61f963b28dae1c18c9c31032e41723ab3
monkey-krk <monkey@monkey.com.hr>
2014-05-20 12:59:05 +0200

    Prifile->MyPlan & MyAccount

--- LifeDojo/Assets/Js/Profile-New-approach.js
+++ LifeDojo/Assets/Js/Profile-New-approach.js
@@ -1,6 +1,4 @@
-﻿/// <reference path="../External/jQuery/jquery-2.1.0.min.js" />
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
 
     if ($("#JournalText").length > 0){
         $("#JournalText").redactor({
@@ -8,8 +6,30 @@ $(document).ready(function () {
         });
     }
 
+    if ($(".MyPlan-Calendar").length > 0) {
 
+        $('.TTab[data-tab="4"]').click(function () {
+            $('.MyPlan-Calendar').fullCalendar({
+                header: {
+                    left: '',
+                    center: 'prev,title,next',
+                    right: ''
+                },
+                firstDay: 1,
+                editable: false,
+                events: CalendarEvents,
+                eventRender: function (event, element, view) {
+                    var today = $.fullCalendar.formatDate(new Date(), 'yyyy-MM-dd');
+                    var dateString = $.fullCalendar.formatDate(event.start, 'yyyy-MM-dd');
+                    if (dateString == today) { element.addClass("Today"); }
+                    element.append("<div class='Description'>" + event.description + "</div>");
+                    element.prepend("<i class='Icon " + event.icon + " " + event.iconcolor + "'></i>")
+                    view.element.find('.fc-day[data-date="' + dateString + '"]').addClass("HasEvent");
+                }
+            });
+        })
 
-
+    }
+    
 
 });
