634604394b80978778a80f22dfe1589125796b8b
monkey-krk <monkey@monkey.com.hr>
2014-06-03 02:07:15 +0200

    Added: Front: AboutUs.html

---
+++ LifeDojo/Assets/Js/Front/AboutUs.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+
+    if ($(".Team").length > 0) {
+
+        $(".Member").mouseenter(function (e) {
+            $(this).find(".Description").fadeIn(200);
+            $(this).addClass("active");
+        })
+        $(".Member").mouseleave(function (e) {
+            $(this).find(".Description").hide();
+            $(this).removeClass("active");
+        })
+        $(".Member").click(function (e) {
+            $(".Member").removeClass("active");
+            $(this).addClass("active");
+            e.stopPropagation();
+        })
+        $(document).click(function () {
+            $(".Member").removeClass("active");
+        });
+
+    }
+
+    if ($(".Programs").length > 0) {
+
+        $(".Program").mouseenter(function (e) {
+            $(this).find(".Description").fadeIn(200);
+            $(this).addClass("active");
+        })
+        $(".Program").mouseleave(function (e) {
+            $(this).find(".Description").hide();
+            $(this).removeClass("active");
+        })
+        $(".Program").click(function (e) {
+            $(".Program").removeClass("active");
+            $(this).addClass("active");
+            e.stopPropagation();
+        })
+        $(document).click(function () {
+            $(".Program").removeClass("active");
+        });
+
+    }
+
+});
