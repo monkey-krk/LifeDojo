a5d92bc9a553581de897fefe09c5bff5d68fb535
monkey-krk <monkey@monkey.com.hr>
2014-06-14 21:13:42 +0200

    ChooseProgram : Auto Ajusting Program Boxes Height

--- LifeDojo/Assets/Js/Dashboard/ChooseProgram.js
+++ LifeDojo/Assets/Js/Dashboard/ChooseProgram.js
@@ -1,3 +1,12 @@
 ﻿$(document).ready(function () {
 
+    //Ajusting Program Boxes height
+    $(".ProgramsList > li:odd").each(function () {
+        var evenH = $(this).prev().height()
+        var oddH = $(this).height()
+        var height = (evenH > oddH ? evenH : oddH);
+        $(this).find(".ProgramWrapper").css('min-height', height + 1 + 'px');
+        $(this).prev().find(".ProgramWrapper").css('min-height', height + 'px');
+    });
+
 });
